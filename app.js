1fc78b2481f4420ad9d00c3cdd150913a8740d15
Rishabh Singh <rishthakur18@gmail.com>
2023-06-09 21:56:27 +0530

    refactor code

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ process.env = require('./.env.js')(process.env.NODE_ENV || 'development');
 
 const port = process.env.PORT || 9000;
 const express = require('express');
-// const https = require('https;')
-// const fs = require('fs');
 
 let indexRoutes = require('./routes/index.js');
 
@@ -14,20 +12,6 @@ const main = async () => {
     app.use(express.urlencoded({ extended: false }));
     app.use('/', indexRoutes);
     app.use('*', (req, res) => res.status(404).send('404 Not Found'));
-    
-    // let key = fs.readFileSync('/tutorial.key','utf-8');
-    // let cert = fs.readFileSync('/tutorial.crt','utf-8');
-    // const parameters = {
-    //     key: key,
-    //     cert: cert
-    //   }
-
-    // let server = https.createServer(parameters,app)
-    
-    // server.listen(port, () => {
-    //     console.log(`Server is listening at port ${port}`) 
-    // });
-
     app.listen(port, () => {
             console.log(`Server is listening at port ${port}`) 
         })
