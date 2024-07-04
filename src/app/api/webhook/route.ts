d25d878be29faed00fce824d7b2088a1753ee957
Rishabh Singh <rishthakur18@gmail.com>
2024-07-04 16:39:59 +0530

    added next code

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,7 +2,35 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json({ message: 'Message sent successfully' });
-}
+    try {
+        const { searchParams } = new URL(request.url);
+        const mode = searchParams.get('hub.mode');
+        const token = searchParams.get('hub.verify_token');
+        const challenge = searchParams.get('hub.challenge');
 
+        if (mode && token && mode === 'subscribe' && process.env.Meta_WA_VerifyToken === token) {
+            return NextResponse.json({ success: true, status: 200 });
+        }
+        else {
+            return NextResponse.json({ success: false, status: 500 });
+        }
+    }
+    catch (error) {
+        console.error({ error })
+        return NextResponse.json({ success: false, status: 500 });
+    }
+};
+
+export async function POST(request: NextRequest) {
+
+    try {
+        console.log(">>> ", request.body);
+        return NextResponse.json({ success: true, status: 200 });
+    }
+    catch (error) {
+        console.error({ error })
+
+        return NextResponse.json({ success: false, status: 500 });
+    }
+};
 
