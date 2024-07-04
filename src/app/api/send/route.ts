7eb19ed2eaa053e8b6dfacd1b1f2fff02f4f4b74
Rishabh Singh <rishthakur18@gmail.com>
2024-07-04 16:41:40 +0530

    added next code

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,9 +1,7 @@
-import send_message from '@/whatsAppUtils/utils';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    send_message();
     return NextResponse.json({ message: 'Message sent successfully' });
 }
 
